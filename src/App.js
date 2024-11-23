09ad5a8b4d8d34499e08a081eec65f68b40f80d4
nipun <nipundilshan991@gmail.com>
2024-11-23 10:37:56 +0530

    grid

--- src/App.js
+++ src/App.js
@@ -32,17 +32,33 @@ function App() {
     <div className="body">
      <div className='top-container'>
       <h1>Animal Matching Game</h1>
+
      </div>
     
      <div className='main-one'>
 
      <div className='left-container'>
+      <h1>Result</h1>
+      <h2>{result && `You ${result}`}</h2>
      </div>
 
-     <div class Name='mid-container'>
+     <div className='mid-container'>
+      <h1>Animal Name</h1>
+      <h2>{randomAnimal}</h2>
      </div>
 
      <div className='right-container'>
+      <div className='grid'>
+        {animals.map((animal,index)=>(
+          <img
+            key={index}
+            src={require(`../src/fig/${animal.img}`)}
+            alt={animal.name}
+            onClick={()=>handleAnimalClick(animal.name)}
+            className='animal-image'
+          />
+        ))}
+      </div>
      </div>
 
      </div>  
